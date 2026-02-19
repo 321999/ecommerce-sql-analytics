f1da858e2b5e876dc73f4176d8b4a93abff7c423
Kishore Jha <kishorejha321999@gmail.com>
2026-02-19 04:13:30 +0000

    order data has been loaded which contain the order_id,item_id,product_id,seller_id,price including freight_value

--- data.sql
+++ data.sql
@@ -91,3 +91,47 @@ truncate olist_geolocation_dataset1;
 
 
 SELECT * FROM olist_geolocation_dataset1 LIMIT 774622, 10;
+
+
+-- ORDER ITEM load the dataset
+-- order_id	order_item_id	product_id	seller_id	shipping_limit_date	price	freight_value
+ /*
+ order id  is the booking id which is generated when the cusotmer placed the order to track the order from both the side customer as well as from bechne vaale ki side
+ order_item_id -- in one order there can be more product orde_tem_id is basically the sequence generated product wise 
+ product_id     each product has its own unified id
+ seller_id      is to track whose the seller of this0 product basically to know from where the customer has recice the produts 
+ shipping_limit_date is the date given by the system to seller to ship the prdouct 
+ price -- each product  has its price of the product
+ freight_value is th shipping cost like something 
+ 
+select length("00010242fe8c5a6d1ba2dd792cb16214");
+select length("4244733e06e7ecb4970a6e2683c13e61");
+select length("48436dade18ac8b2bce089ec2a041202");
+2017-09-19 09:45:35
+-- freight and price both are teh decimal values 
+
+timestamp dataset  yyyy:mm:dd hh:mm:ss
+*/
+use olis;
+create table olist_order_items(
+orderid varchar(50),
+item_id int,-- product squence in order
+product_id varchar(50),
+shipping_limit_date timestamp,
+price decimal(8,3),
+freight_value decimal(5,3)-- delivery charges 
+);
+-- seller id column has not added 
+alter table olist_order_items add column seller_id varchar(50) after product_id;
+
+load data infile '/var/lib/mysql-files/olist_order_items_dataset.csv'
+into table olist_order_items
+fields terminated by  ','
+enclosed by '"'
+lines terminated by '\n'
+ignore 1 rows;
+select * from olist_order_items;
+-- freight value is out of range 
+-- changing the datatype of freight value 
+alter table olist_order_items 
+modify column freight_value decimal(6,3);
